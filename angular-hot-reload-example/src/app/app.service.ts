25f367e08206d12623a51f6940cd5001b8a92b1b
ebinezer <developer@clipbook.io>
2023-08-03 23:23:09 +0530

    docker-compose is working for dev and prod

--- angular-hot-reload-example/src/app/app.service.ts
+++ angular-hot-reload-example/src/app/app.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
+  rootUrl!: string;
   constructor(private httpClient: HttpClient) {
+    this.rootUrl = environment.apiUrl;
   }
 
   getHello(){
-    return this.httpClient.get<{data:string} >('/api/hello');
+    return this.httpClient.get<{data:string} >(this.rootUrl+'/hello');
   }
 }
